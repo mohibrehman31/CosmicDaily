70b8bb0b2a31b42cffcb830b9abc9a8eb1a5ac47
mohibrehman31 <mohibrehman31@gmail.com>
2024-10-02 23:58:30 +0100

    L

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
+const app = require('./app');
+const config = require('./config/config');
+const logger = require('./config/logger');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+let server;
+
+server = app.listen(config.port, () => {
+  logger.info(`Listening to port ${config.port}`);
 });
 
-// Add your other routes and middleware here
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
 
-// Important: For Vercel deployment, we export the app instead of calling app.listen()
-module.exports = app;
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
