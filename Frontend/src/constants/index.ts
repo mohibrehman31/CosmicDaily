d09fde36604b0912fb7cb2743f925f6273213735
mohibrehman31 <mohibrehman31@gmail.com>
2024-10-06 22:20:19 +0100

    Updated

--- Frontend/src/constants/index.ts
+++ Frontend/src/constants/index.ts
@@ -1,7 +1,6 @@
 import {
   benefitIcon1,
   benefitIcon2,
-  benefitIcon3,
   benefitIcon4,
   benefitImage2,
   chromecast,
@@ -402,23 +401,6 @@ export const benefits = [
   },
   {
     id: "1",
-    title: "Improve everyday",
-    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
-    backgroundUrl: "./src/assets/benefits/card-2.svg",
-    iconUrl: benefitIcon2,
-    imageUrl: benefitImage2,
-    light: true,
-  },
-  {
-    id: "2",
-    title: "Connect everywhere",
-    text: "Connect with the AI chatbot from anywhere, on any device, making it more accessible and convenient.",
-    backgroundUrl: "./src/assets/benefits/card-3.svg",
-    iconUrl: benefitIcon3,
-    imageUrl: benefitImage2,
-  },
-  {
-    id: "3",
     title: "Fast responding",
     text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
     backgroundUrl: "./src/assets/benefits/card-4.svg",
@@ -427,7 +409,7 @@ export const benefits = [
     light: true,
   },
   {
-    id: "4",
+    id: "2",
     title: "Ask anything",
     text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
     backgroundUrl: "./src/assets/benefits/card-5.svg",
@@ -435,12 +417,13 @@ export const benefits = [
     imageUrl: benefitImage2,
   },
   {
-    id: "5",
+    id: "3",
     title: "Improve everyday",
     text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
     backgroundUrl: "./src/assets/benefits/card-6.svg",
     iconUrl: benefitIcon2,
     imageUrl: benefitImage2,
+    light: true,
   },
 ];
 
